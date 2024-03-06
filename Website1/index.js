f19b14574611ca87bcf1e2ef2f588297618f419d
Constantine Turov <constantine.turov@lionbridge.com>
2024-03-06 06:40:26 -0500

    Arrow Functions

--- Website1/index.js
+++ Website1/index.js
@@ -193,3 +193,25 @@ greetingName("Valentin");
 
 let grades = [100, 39, 29, 4];
 grades.forEach((a) => console.log(a));
+
+
+/************************************** SHUFFLE CARDS */
+
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+shuffle(cards);
+
+console.log(cards);
+
+function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * array.length);
+        currentIndex -=1;
+
+        let temp = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
+    return array;
+}
