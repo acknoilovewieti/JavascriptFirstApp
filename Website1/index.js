371ccff64b9ef1c2a9ba2b270efc20d85bcabceb
Constantine Turov <constantine.turov@lionbridge.com>
2024-07-02 14:27:50 -0400

    Promise

--- Website1/index.js
+++ Website1/index.js
@@ -487,3 +487,18 @@ function countUp(){
         clearInterval(myTimer);
     }
 }
+
+/************* promise ************/
+
+const promise = new Promise((resolve, reject) => {
+    let fileLoaded = false;
+    if(fileLoaded){
+        resolve("File loaded");
+    }
+    else{
+        reject("File NOT loaded");
+    }
+});
+
+promise.then(value => console.log(value))
+    .catch(error => console.log(error));
